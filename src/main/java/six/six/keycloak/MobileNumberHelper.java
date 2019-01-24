d53dbed39beb29e7b942b655fbfeb315111aee9c
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-01-24 14:59:40 +0000

    MSPSDS-896 Use getFirstAttribute to simply helper method

--- src/main/java/six/six/keycloak/MobileNumberHelper.java
+++ src/main/java/six/six/keycloak/MobileNumberHelper.java
@@ -2,16 +2,14 @@ package six.six.keycloak;
 
 import org.keycloak.models.UserModel;
 
-import java.util.List;
-
 public class MobileNumberHelper {
     public static String getMobileNumber(UserModel user) {
-        List<String> mobileNumberCreds = user.getAttribute(KeycloakSmsConstants.ATTR_MOBILE);
+        String mobileNumberCreds = user.getFirstAttribute(KeycloakSmsConstants.ATTR_MOBILE);
 
         String mobileNumber = null;
 
         if (mobileNumberCreds != null && !mobileNumberCreds.isEmpty()) {
-            mobileNumber = mobileNumberCreds.get(0);
+            mobileNumber = mobileNumberCreds;
         }
 
         return mobileNumber;
